6d8482d358dbb8674d98ac43dc3a4b8cfa07cbf8
Unknown <janis.juppe@gmail.com>
2018-04-20 16:59:38 +0200

    Included excel import

--- Testing.py
+++ Testing.py
@@ -1,6 +1,4 @@
 class Individual:
-
-
     def __init__(self, id):
         self.id = id
         self.aircraft = []
@@ -19,7 +17,17 @@ class Individual:
             self.runway.append(random.randint(1, self.numberOfRunways))
 
 
-
 x = Individual(1)
 x.createRandom()
 print(x.proportion)
+
+
+import pandas
+
+df = pandas.read_excel("C:\\Users\\janis\\Documents\\Uni\\Master\\TUM\\Airport Operations Management\\Airland1.xlsx")
+
+print(df.columns)
+
+values = df[3].values
+
+print(values)
