64297d8ca4954c6e74220a25febd2e277a66c06b
Unknown <janis.juppe@gmail.com>
2018-05-05 14:45:41 +0200

    Implement algorithm
    Implemented the initial heuristic creation, missing including runway
    implemented a class structure

--- Individual.py
+++ Individual.py
@@ -1,71 +1,138 @@
 import pandas
 import numpy
+from AirplaneLandings.Chromosome import Airplane
+import operator
 
 
 class Individual:
     def __init__(self, id):
         self.id = id
-        self.aircraft = []
-        self.proportion = []
-        self.runway = []
-        self.earliestTime = []
-        self.targetTime = []
-        self.landingTime = {}
-        self.latestTime = []
-        self.penaltyCost = []
-        self.objectiveFunctionValue = 0
-        self.unfitness = 0
-        self.createRandom()
+        self.fitness = 0.0
+        self.unfitness = 0.0
+        self.airplanes = []
 
     def __repr__(self):
         return repr(
             "The unfitness of individual " + str(self.id) + " is " + str(self.unfitness) + " and obj is: " + str(
-                self.objectiveFunctionValue))
+                self.fitness))
 
     numberOfAircrafts = 10
     numberOfRunways = 1
-    directory = "C:\\Users\\janis\\Documents\\Uni\\Master\\TUM\\Airport Operations Management\\Data\\Airland1.xlsx"
+    directory = "C:\\Users\\janis\\Documents\\Uni\\Master\\TUM\\Airport Operations Management\\Bionomic Algorithm\\Data\\Airland1.xlsx"
     minDistanceBetweenPlane = numpy.zeros((numberOfAircrafts, numberOfAircrafts))
 
     def createRandom(self):
         import random
         for x in range(self.numberOfAircrafts):
-            self.proportion.append(random.random())
+            self.airplanes.append(Airplane(x))
+            self.airplanes[x].proportion = random.random()
+            self.airplanes[x].runwayAllocated = random.randint(1, self.numberOfRunways)
 
-        for y in range(self.numberOfAircrafts):
-            self.runway.append(random.randint(1, self.numberOfRunways))
-
-        self.calcTime()
+        self.importTimes()
         self.calcLandingTime()
         self.calcObjectiveFunction()
         self.calcUnfitnessValue()
 
-    def calcTime(self):
+    def createHeuristicIndividual(self, type):
+        if type == HeuristicTypes.EARLIEST:
+            self.createEarliestHeuristic()
+        if type == HeuristicTypes.TARGET:
+            self.createTargetHeuristic()
+        if type == HeuristicTypes.LATEST:
+            self.createLatesttHeuristic()
+
+    def createEarliestHeuristic(self):
+        for x in range(Individual.numberOfAircrafts):
+            self.airplanes.append(Airplane(x))
+
+        self.importTimes()
+        earliest_Sorted = sorted(self.airplanes, key=lambda airplane: airplane.earliestTime)
+
+        earliest_Sorted[0].landingTime = earliest_Sorted[0].earliestTime
+        earliest_Sorted[0].proportion = (earliest_Sorted[0].landingTime - earliest_Sorted[0].earliestTime) / (
+                earliest_Sorted[0].latestTime - earliest_Sorted[0].earliestTime)
+
+        for x in range(1, Individual.numberOfAircrafts, 1):
+            current = earliest_Sorted[x]
+            bufferToPrevious = earliest_Sorted[x - 1].landingTime + \
+                               self.minDistanceBetweenPlane[earliest_Sorted[x - 1].aircraftNr - 1][
+                                   current.aircraftNr - 1]
+            current.landingTime = bufferToPrevious if bufferToPrevious > current.earliestTime else current.earliestTime
+            current.proportion = (current.landingTime - current.earliestTime) / (
+                    current.latestTime - current.earliestTime)
+
+        self.calcObjectiveFunction()
+        self.calcUnfitnessValue()
+
+    def createTargetHeuristic(self):
+        for x in range(Individual.numberOfAircrafts):
+            self.airplanes.append(Airplane(x))
+
+        self.importTimes()
+        target_Sorted = sorted(self.airplanes, key=lambda airplane: airplane.targetTime)
+
+        target_Sorted[0].landingTime = target_Sorted[0].targetTime
+        target_Sorted[0].proportion = (target_Sorted[0].landingTime - target_Sorted[0].earliestTime) / (
+                target_Sorted[0].latestTime - target_Sorted[0].earliestTime)
+        for x in range(1, Individual.numberOfAircrafts, 1):
+            current = target_Sorted[x]
+            bufferToPrevious = target_Sorted[x - 1].landingTime + \
+                               self.minDistanceBetweenPlane[target_Sorted[x - 1].aircraftNr - 1][current.aircraftNr - 1]
+            current.landingTime = bufferToPrevious if bufferToPrevious > current.earliestTime else current.earliestTime
+            current.proportion = (current.landingTime - current.earliestTime) / (
+                    current.latestTime - current.earliestTime)
+
+        self.calcObjectiveFunction()
+        self.calcUnfitnessValue()
+
+    def createLatesttHeuristic(self):
+        for x in range(Individual.numberOfAircrafts):
+            self.airplanes.append(Airplane(x))
+
+        self.importTimes()
+        latest_Sorted = sorted(self.airplanes, key=lambda airplane: airplane.latestTime)
+
+        latest_Sorted[0].landingTime = latest_Sorted[0].latestTime
+        latest_Sorted[0].proportion = (latest_Sorted[0].landingTime - latest_Sorted[0].earliestTime) / (
+                latest_Sorted[0].latestTime - latest_Sorted[0].earliestTime)
+        for x in range(1, Individual.numberOfAircrafts, 1):
+            current = latest_Sorted[x]
+            bufferToPrevious = latest_Sorted[x - 1].landingTime + \
+                               self.minDistanceBetweenPlane[latest_Sorted[x - 1].aircraftNr - 1][current.aircraftNr - 1]
+            current.landingTime = bufferToPrevious if bufferToPrevious > current.earliestTime else current.earliestTime
+            current.proportion = (current.landingTime - current.earliestTime) / (
+                    current.latestTime - current.earliestTime)
+
+        self.calcObjectiveFunction()
+        self.calcUnfitnessValue()
+
+    def importTimes(self):
         df = pandas.read_excel(Individual.directory)
         array = df.as_matrix()
-        print(list(df))
         for x in range(Individual.numberOfAircrafts):
-            self.earliestTime.append(array[x][5])
-            self.latestTime.append(array[x][7])
-            self.penaltyCost.append(array[x][8])
-            self.targetTime.append(array[x][6])
-            for y in range(Individual.numberOfAircrafts-1):
+            self.airplanes[x].earliestTime = (array[x][5])
+            self.airplanes[x].latestTime = (array[x][7])
+            self.airplanes[x].penaltyCost = (array[x][8])
+            self.airplanes[x].targetTime = (array[x][6])
+            for y in range(Individual.numberOfAircrafts):
                 self.minDistanceBetweenPlane[x][y] = array[x][10 + y]
 
     def calcLandingTime(self):
         for x in range(Individual.numberOfAircrafts):
-            earliest = self.earliestTime[x]
-            latest = self.latestTime[x]
-            self.landingTime[x] = (earliest + self.proportion[x] * (latest - earliest))
+            earliest = self.airplanes[x].earliestTime
+            latest = self.airplanes[x].latestTime
+            self.airplanes[x].landingTime = (earliest + self.airplanes[x].proportion * (latest - earliest))
 
     def calcObjectiveFunction(self):
         for x in range(Individual.numberOfAircrafts):
-            penalty = abs(self.landingTime[x] - self.targetTime[x]) * self.penaltyCost[x]
-            self.objectiveFunctionValue += penalty
+            penalty = abs(self.airplanes[x].landingTime - self.airplanes[x].targetTime) * self.airplanes[x].penaltyCost
+            self.fitness += penalty
 
     def calcUnfitnessValue(self):
-        import operator
-        sorted_landingTimes = sorted(self.landingTime.items(), key=operator.itemgetter(1))
+        landingTimes = {}
+        for x in range(Individual.numberOfAircrafts):
+            landingTimes[x] = self.airplanes[x].landingTime
+        sorted_landingTimes = sorted(landingTimes.items(), key=operator.itemgetter(1))
 
         for x in range(0, (Individual.numberOfAircrafts - 1)):
             a = abs(sorted_landingTimes[x][1] - sorted_landingTimes[x + 1][1])
@@ -76,3 +143,8 @@ class Individual:
         return self.id == other.id
 
 
+def enum(**named_values):
+    return type('Enum', (), named_values)
+
+
+HeuristicTypes = enum(EARLIEST='1', TARGET='2', LATEST='3')
--- Population.py
+++ Population.py
@@ -5,26 +5,22 @@ class Population:
     def __init__(self, populationSize):
         self.individuals = []
         self.rankedIndividuals = []
-        self.inclusionValue = [populationSize]
-        for x in range(populationSize):
+        self.inclusionValue = []
+        for x in range(populationSize - 3):
             self.individuals.append(Individual(x))
+            self.individuals[x].createRandom()
 
     def rankIndividuals(self):
         self.rankedIndividuals = sorted(self.individuals,
-                                        key=lambda indiv: (indiv.unfitness, -indiv.objectiveFunctionValue))
+                                        key=lambda indiv: (indiv.unfitness, -indiv.fitness))
         for x in range(len(self.rankedIndividuals) - 1, 0, -1):
             self.inclusionValue.append(x)
 
     def calcDistance(self, lhs: Individual, rhs: Individual):
         totaldistance = 0
         for x in range(lhs.numberOfAircrafts):
-            if lhs.runway[x] != rhs.runway[x]:
+            if lhs.airplanes[x].runway != rhs.airplanes[x].runway:
                 totaldistance += 1
             else:
-                totaldistance += abs(lhs.proportion[x] - rhs.proportion[x])
+                totaldistance += abs(lhs.airplanes[x].proportion - rhs.airplanes[x].proportion)
         return totaldistance
-
-
-b = Population(15)
-
-print(b.calcDistance(b.individuals[1], b.individuals[2]))
